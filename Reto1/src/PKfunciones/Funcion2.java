96b3e16c416ca53d750de3f2ef9c04b3dabe7d40
DAM1 <DAM1@aula11-pc18>
2024-10-29 10:31:39 +0100

    He hecho la funcion Dividir

--- Reto1/src/PKfunciones/Funcion2.java
+++ Reto1/src/PKfunciones/Funcion2.java
@@ -8,6 +8,11 @@ public class Funcion2 {
 	public static int multiplicar(int n1,int n2) {
 		return n1*n2;
 	}
-	
+	public static int dividir(int n1,int n2) {
+		if (n2!=0) {
+			return n1/n2;
+		}
+		return 0;
+	}
 	
 }
