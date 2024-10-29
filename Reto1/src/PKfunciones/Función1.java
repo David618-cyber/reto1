a04cec511bb10447f98eba1ed5bbb54f237b64fc
david618-cyber <davidjimeno077@gmail.com>
2024-10-29 10:36:28 +0100

    Creadas funciones 1

--- Reto1/src/PKfunciones/Función1.java
+++ Reto1/src/PKfunciones/Función1.java
@@ -1,6 +1,29 @@
 package PKfunciones;
 
-public class Función1 {}
-
-
+public class Función1 {
+	public static int sumar(int n1,int n2) {
+		return n1+n2;
+		
+	}
+	public static int resta(int n1,int n2) {
+		return n1-n2;
+	}
+	public static boolean esPar(int n1) {
+		if(n1%2==0) {
+		return true;
+		}
+		return false;
+	} 
+	public static boolean esImpar(int n1) {
+		if(n1%2!=0) {
+		return true;
+		}
+		return false;
+	} 
+	
 }
+
+
+
+
+
